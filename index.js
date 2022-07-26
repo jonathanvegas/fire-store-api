312af235b018c665743e92f652234eef8af7a7eb
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-26 17:14:08 -0400

    created simple express app

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
 import express from "express";
-import { getAllCars} from "./src/cars.js"
+import { createCar, getAllCars} from "./src/cars.js"
 
 const app = express();
 const PORT = 3002;
 app.use(express.json());
 
 // put our routes here
-app.get("/", (req, res) => {
-  res.send("!!!Express is working!!!");
-});
-
 app.get('/cars', getAllCars);
+app.post('/cars', createCar);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}...`);
 });
+
+// app.get("/", (req, res) => {
+//   res.send("!!!Express is working!!!");
+// });
+
--- src/cars.js
+++ src/cars.js
@@ -1,15 +1,45 @@
-import { dbConnect } from './dbConnect.js'
+import { dbConnect } from "./dbConnect.js";
 
 export function getAllCars(req, res) {
   //connect to db (putting it inside the function makes sure to be connected cause it calls it everytime)
-  const db = dbConnect()
+  const newCar = req.body;
+  const db = dbConnect();
   //get all docs from cars collection
-  db.collection('cars').get()
+  db.collection("cars")
+    .get()
     .then((collection) => {
       //reshape collection to array
-      const cars = collection.docs.map((doc) => doc.data())
+      const cars = collection.docs.map((doc) => doc.data());
       //send array to response
-      res.send(cars)
+      res.send(cars);
     })
-    .catch((err) => res.status(500).send(err))
+    .catch((err) => res.status(500).send(err));
 }
+
+export function createCar(req, res) {
+  // get a new car from request body
+  const newCar = req.body;
+  // connect to database
+  const db = dbConnect();
+  // add that car to car collection
+  db.collection('cars').add(newCar)
+    .then(doc => {
+        res.status(201).send({
+            success: true,
+            id: doc.id
+        })
+    })
+    .catch(err => res.status(500).send(err)) //could be change for handleError function
+  // send back new doc id
+}
+
+// get function updateCar(req,res){
+//     const {id} = req.params
+//     //connect db
+//     const 
+// }
+
+// function handleError (err,res) {
+//     //console.
+//     res.status(500).send(err);
+// }
