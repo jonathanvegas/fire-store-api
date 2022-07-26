4adb1f5541004406eece2b366c83177ec9ee40f1
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-26 14:40:19 -0400

    created simple express app

---
+++ index.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+const PORT = 3002;
+app.use(express.json());
+
+// put our routes here
+app.get("/", (req, res) => {
+  res.send("!!!Express is working!!!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}...`);
+});
+
