8bf9fd158bdfa287ce4ed1eec278d33b3a4162db
Jonathan Vegas <jonathanvegas@Jonathans-MacBook-Air.local>
2022-07-27 09:21:44 -0400

    created simple express app

--- src/cars.js
+++ src/cars.js
@@ -1,45 +1,59 @@
-import { dbConnect } from "./dbConnect.js";
+import { dbConnect } from './dbConnect.js'
 
 export function getAllCars(req, res) {
   //connect to db (putting it inside the function makes sure to be connected cause it calls it everytime)
-  const newCar = req.body;
-  const db = dbConnect();
+  const db = dbConnect()
   //get all docs from cars collection
-  db.collection("cars")
+  db.collection('cars')
     .get()
     .then((collection) => {
       //reshape collection to array
-      const cars = collection.docs.map((doc) => doc.data());
+      const cars = collection.docs.map((doc) => doc.data())
       //send array to response
-      res.send(cars);
+      res.send(cars)
     })
-    .catch((err) => res.status(500).send(err));
+    .catch((err) => res.status(500).send(err)) //this is a very typical error catcher
 }
 
 export function createCar(req, res) {
-  // get a new car from request body
-  const newCar = req.body;
-  // connect to database
-  const db = dbConnect();
-  // add that car to car collection
-  db.collection('cars').add(newCar)
-    .then(doc => {
-        res.status(201).send({
-            success: true,
-            id: doc.id
-        })
+  //get a new car from request body
+  const newCar = req.body
+  //connect to db
+  const db = dbConnect()
+  //add that car to cars collection
+  db.collection('cars')
+    .add(newCar)
+    .then((doc) => {
+      res.status(201).send({
+        success: true,
+        id: doc.id,
+      })
     })
-    .catch(err => res.status(500).send(err)) //could be change for handleError function
-  // send back new doc id
+    .catch((err) => handleError(err, res)) //this error is handled by a function that is another way to handle them
+  //send back new doc id
 }
 
-// get function updateCar(req,res){
-//     const {id} = req.params
-//     //connect db
-//     const 
-// }
+export function updateCar (req, res) {
+    const { id } = req.params
+    let carToUpdate = req.body
+    //connect db
+    const db = dbConnect()
+    //update doc(id) in cars collection using req.body
+    db.collection('cars')
+        .doc(id)
+        .update(carToUpdate)
+        .then((doc) => {
+        res.status(201).send({
+            success: true,
+            id: doc.id,
+        })
+    })
+    .catch((err) => handleError(err, res))
+}
 
-// function handleError (err,res) {
-//     //console.
-//     res.status(500).send(err);
-// }
+//this is another way to catch errors
+function handleError(err, res) {
+  res.status(500).send(err)
+}
+
+
--- src/dbConnect.js
+++ src/dbConnect.js
@@ -3,10 +3,10 @@ import { getFirestore } from "firebase-admin/firestore";
 import { credentials } from "../credentials.js";
 
 export function dbConnect() {
-  if (!getApps().length) {
+  if(!getApps().length){
     initializeApp({
-      credential: cert(credentials),
-    });
-    return getFirestore();
-  }
+      credential: cert(credentials)
+    });  
+}
+  return getFirestore();
 }
